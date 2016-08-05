c9868b68715e1f68a8d3f6d5770a3ba12038a84f
kiddos <josephyu831007@yahoo.com.tw>
2016-08-05 14:32:43 +0800

    checkout python3

--- run_test.sh
+++ run_test.sh
@@ -7,12 +7,12 @@ TEST_C=test/test.c
 TEST_CPP=test/test.cpp
 
 
-python $TEST_PROGRAM $TEST_C 11 1 c11 "printf"
-python $TEST_PROGRAM $TEST_C 22 33 c11 "'i', TypedText"
-python $TEST_PROGRAM $TEST_C 22 38 c11 "'i', TypedText"
-python $TEST_PROGRAM $TEST_CPP 31 9 c++11 "test_func1"
-python $TEST_PROGRAM $TEST_CPP 33 11 c++11 "geta"
-python $TEST_PROGRAM $TEST_CPP 33 25 c++11 "getd"
-python $TEST_PROGRAM $TEST_CPP 34 9 c++11 "getsize"
-python $TEST_PROGRAM $TEST_CPP 34 19 c++11 "'width', TypedText"
-python $TEST_PROGRAM $TEST_CPP 34 40 c++11 "'height', TypedText"
+python3 $TEST_PROGRAM $TEST_C 11 1 c11 "printf"
+python3 $TEST_PROGRAM $TEST_C 22 33 c11 "'i', TypedText"
+python3 $TEST_PROGRAM $TEST_C 22 38 c11 "'i', TypedText"
+python3 $TEST_PROGRAM $TEST_CPP 31 9 c++11 "test_func1"
+python3 $TEST_PROGRAM $TEST_CPP 33 11 c++11 "geta"
+python3 $TEST_PROGRAM $TEST_CPP 33 25 c++11 "getd"
+python3 $TEST_PROGRAM $TEST_CPP 34 9 c++11 "getsize"
+python3 $TEST_PROGRAM $TEST_CPP 34 19 c++11 "'width', TypedText"
+python3 $TEST_PROGRAM $TEST_CPP 34 40 c++11 "'height', TypedText"
