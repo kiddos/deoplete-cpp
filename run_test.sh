0a494ea039d3b07fff0f781ab6b15c409f85ba8f
kiddos <josephyu831007@yahoo.com.tw>
2016-08-06 12:31:45 +0800

    add cmake test

--- run_test.sh
+++ run_test.sh
@@ -2,17 +2,21 @@
 
 set -e
 
-TEST_PROGRAM=test/test_code_complete.py
+PY=python3
+TEST_CODE_COMPLETION=test/test_code_complete.py
 TEST_C=test/test.c
 TEST_CPP=test/test.cpp
+TEST_CMAKE=test/test_cmake_command.py
 
 
-python3 $TEST_PROGRAM $TEST_C 11 1 c11 "printf"
-python3 $TEST_PROGRAM $TEST_C 22 33 c11 "'i', TypedText"
-python3 $TEST_PROGRAM $TEST_C 22 38 c11 "'i', TypedText"
-python3 $TEST_PROGRAM $TEST_CPP 31 9 c++11 "test_func1"
-python3 $TEST_PROGRAM $TEST_CPP 33 11 c++11 "geta"
-python3 $TEST_PROGRAM $TEST_CPP 33 25 c++11 "getd"
-python3 $TEST_PROGRAM $TEST_CPP 34 9 c++11 "getsize"
-python3 $TEST_PROGRAM $TEST_CPP 34 19 c++11 "'width', TypedText"
-python3 $TEST_PROGRAM $TEST_CPP 34 40 c++11 "'height', TypedText"
+$PY $TEST_CODE_COMPLETION $TEST_C 11 1 c11 "printf"
+$PY $TEST_CODE_COMPLETION $TEST_C 22 33 c11 "'i', TypedText"
+$PY $TEST_CODE_COMPLETION $TEST_C 22 38 c11 "'i', TypedText"
+$PY $TEST_CODE_COMPLETION $TEST_CPP 31 9 c++11 "test_func1"
+$PY $TEST_CODE_COMPLETION $TEST_CPP 33 11 c++11 "geta"
+$PY $TEST_CODE_COMPLETION $TEST_CPP 33 25 c++11 "getd"
+$PY $TEST_CODE_COMPLETION $TEST_CPP 34 9 c++11 "getsize"
+$PY $TEST_CODE_COMPLETION $TEST_CPP 34 19 c++11 "'width', TypedText"
+$PY $TEST_CODE_COMPLETION $TEST_CPP 34 40 c++11 "'height', TypedText"
+
+$PY  $TEST_CMAKE
