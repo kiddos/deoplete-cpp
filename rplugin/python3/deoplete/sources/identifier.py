adae51cbe7c17863af148fb187902a675c8ca002
kiddos <kiddo831007@gmail.com>
2019-02-08 00:17:58 +0800

    format code

--- rplugin/python3/deoplete/sources/identifier.py
+++ rplugin/python3/deoplete/sources/identifier.py
@@ -2,24 +2,24 @@ import re
 from .base import Base
 
 class Source(Base):
-    def __init__(self, vim):
-        Base.__init__(self, vim)
+  def __init__(self, vim):
+    Base.__init__(self, vim)
 
-        self.name = 'identifier'
-        self.mark = '[I]'
-        self.rank = 100
-        self.debug_enabled = False
-        self._cache = []
+    self.name = 'identifier'
+    self.mark = '[ID]'
+    self.rank = 100
+    self.debug_enabled = False
+    self._cache = []
 
-        self._gather_identifier()
+    self._gather_identifier()
 
-    def _gather_identifier(self):
-        for w in re.findall(r'[\w\d]+', '\n'.join(self.vim.current.buffer)):
-            if w not in self._cache:
-                self._cache.append(w)
+  def _gather_identifier(self):
+    for w in re.findall(r'[\w\d]+', '\n'.join(self.vim.current.buffer)):
+      if w not in self._cache:
+        self._cache.append(w)
 
-    def on_event(self, context):
-        self._gather_identifier()
+  def on_event(self, context):
+    self._gather_identifier()
 
-    def gather_candidates(self, context):
-        return [{'word': w, 'kind': 'identifier'} for w in self._cache]
+  def gather_candidates(self, context):
+    return [{'word': w, 'kind': 'identifier'} for w in self._cache]
