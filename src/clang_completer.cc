af4b4048ab4f5defb134967a47f845afcf695f72
kiddos <kiddo831007@gmail.com>
2019-02-07 22:36:26 +0800

    add clang completer

---
+++ src/clang_completer.cc
@@ -0,0 +1,205 @@
+#include "clang_completer.h"
+
+// excludeDeclarationsFromPCH: allows enumeration of "local" declarations (when
+// loading any new translation units)
+ClangCompleter::ClangCompleter() : index_(clang_createIndex(1, 1)) {
+  parse_option_ = CXTranslationUnit_DetailedPreprocessingRecord |
+                  CXTranslationUnit_Incomplete |
+                  CXTranslationUnit_PrecompiledPreamble |
+                  CXTranslationUnit_CacheCompletionResults |
+                  CXTranslationUnit_SkipFunctionBodies |
+                  CXTranslationUnit_CreatePreambleOnFirstParse |
+                  CXTranslationUnit_KeepGoing;
+
+  complete_option_ = CXCodeComplete_IncludeMacros |
+                     CXCodeComplete_IncludeCompletionsWithFixIts;
+}
+
+ClangCompleter::~ClangCompleter() {
+  for (int i = 0; i < tus_.size(); ++i) {
+    clang_disposeTranslationUnit(tus_[i]);
+  }
+  clang_disposeIndex(index_);
+}
+
+void ClangCompleter::Reparse(const std::string& file,
+                             const std::string& content) {
+  auto it = std::find(files_.begin(), files_.end(), file);
+  if (it == files_.end()) {
+    AddFile(file, content, CPPArgumentManager());
+  } else {
+    CXUnsavedFile unsaved_files{
+        file.c_str(),
+        content.c_str(),
+        content.length(),
+    };
+
+    int index = std::distance(files_.begin(), it);
+    clang_reparseTranslationUnit(tus_[index], 1, &unsaved_files, parse_option_);
+  }
+}
+
+bool ClangCompleter::HasFile(const std::string& file) {
+  return std::find(files_.begin(), files_.end(), file) != files_.end();
+}
+
+void ClangCompleter::AddFile(const std::string& file,
+                             const ArgumentManager& arg_manager) {
+  if (!HasFile(file)) {
+    std::vector<char*> args;
+    arg_manager.PrepareArgs(args);
+
+    std::ifstream input_file(file);
+    if (input_file.is_open()) {
+      CXTranslationUnit tu =
+          clang_parseTranslationUnit(index_, file.c_str(), &args[0],
+                                     args.size(), nullptr, 0, parse_option_);
+
+      tus_.push_back(tu);
+      files_.push_back(file);
+    }
+  }
+}
+
+void ClangCompleter::AddFile(const std::string& file,
+                             const std::string& content,
+                             const ArgumentManager& arg_manager) {
+  if (!HasFile(file)) {
+    CXUnsavedFile unsaved_files{
+        file.c_str(),
+        content.c_str(),
+        content.length(),
+    };
+
+    std::vector<char*> args;
+    arg_manager.PrepareArgs(args);
+
+    CXTranslationUnit tu =
+        clang_parseTranslationUnit(index_, file.c_str(), &args[0], args.size(),
+                                   &unsaved_files, 1, parse_option_);
+    tus_.push_back(tu);
+    files_.push_back(file);
+  }
+}
+
+std::string ClangCompleter::GetFileContent(const std::string& file) {
+  std::ifstream input_stream(file);
+  std::string content((std::istreambuf_iterator<char>(input_stream)),
+                      std::istreambuf_iterator<char>());
+  return content;
+}
+
+std::vector<ClangCompleter::Result> ClangCompleter::CodeComplete(
+    const std::string& file, const std::string& content, int line, int column,
+    const ArgumentManager& arg_manager) {
+  if (!HasFile(file)) {
+    AddFile(file, content, arg_manager);
+  }
+
+  auto it = std::find(files_.begin(), files_.end(), file);
+  int index = std::distance(files_.begin(), it);
+
+  CXTranslationUnit tu = tus_[index];
+  CXUnsavedFile unsaved_files{
+      file.c_str(),
+      content.c_str(),
+      content.length(),
+  };
+
+  CXCodeCompleteResults* results = clang_codeCompleteAt(
+      tu, file.c_str(), line, column, &unsaved_files, 1, complete_option_);
+
+  std::vector<Result> outputs;
+  if (results) {
+    for (int i = 0; i < results->NumResults; ++i) {
+      CXCompletionString cs = results->Results[i].CompletionString;
+      Result result = GetResult(cs);
+      outputs.push_back(result);
+    }
+  }
+  clang_disposeCodeCompleteResults(results);
+
+  return outputs;
+}
+
+ClangCompleter::Result ClangCompleter::GetResult(CXCompletionString cs) {
+  Result result;
+  int n = clang_getNumCompletionChunks(cs);
+  for (int i = 0; i < n; ++i) {
+    CXCompletionChunkKind chunk_kind = clang_getCompletionChunkKind(cs, i);
+
+    std::string kind;
+    switch (chunk_kind) {
+      case CXCompletionChunk_Optional:
+        kind = "Optional";
+        break;
+      case CXCompletionChunk_TypedText:
+        kind = "TypedText";
+        break;
+      case CXCompletionChunk_Text:
+        kind = "Text";
+        break;
+      case CXCompletionChunk_Placeholder:
+        kind = "Placeholder";
+        break;
+      case CXCompletionChunk_Informative:
+        kind = "Information";
+        break;
+      case CXCompletionChunk_CurrentParameter:
+        kind = "CurrentParameter";
+        break;
+      case CXCompletionChunk_LeftParen:
+        kind = "LeftParen";
+        break;
+      case CXCompletionChunk_RightParen:
+        kind = "RightParen";
+        break;
+      case CXCompletionChunk_LeftBracket:
+        kind = "LeftBracket";
+        break;
+      case CXCompletionChunk_RightBracket:
+        kind = "RightBracket";
+        break;
+      case CXCompletionChunk_LeftBrace:
+        kind = "LeftBrace";
+        break;
+      case CXCompletionChunk_RightBrace:
+        kind = "RightBrace";
+        break;
+      case CXCompletionChunk_LeftAngle:
+        kind = "LeftAngle";
+        break;
+      case CXCompletionChunk_RightAngle:
+        kind = "RightAngle";
+        break;
+      case CXCompletionChunk_Comma:
+        kind = "Comma";
+        break;
+      case CXCompletionChunk_ResultType:
+        kind = "ResultType";
+        break;
+      case CXCompletionChunk_Colon:
+        kind = "Colon";
+        break;
+      case CXCompletionChunk_SemiColon:
+        kind = "SemiColon";
+        break;
+      case CXCompletionChunk_Equal:
+        kind = "Equal";
+        break;
+      case CXCompletionChunk_HorizontalSpace:
+        kind = "HorizontalSpace";
+        break;
+      case CXCompletionChunk_VerticalSpace:
+        kind = "VerticalSpace";
+        break;
+    }
+
+    CXString text = clang_getCompletionChunkText(cs, i);
+    const char* completion = reinterpret_cast<const char*>(text.data);
+    std::string complete_content(completion);
+
+    result.push_back(std::make_pair(kind, complete_content));
+  }
+  return result;
+}
---
+++ src/clang_completer.h
@@ -0,0 +1,43 @@
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include <clang-c/Index.h>
+
+#include "argument_manager.h"
+#include "cpp_argument_manager.h"
+
+class ClangCompleter {
+ public:
+  typedef std::pair<std::string, std::string> ResultKind;
+  typedef std::vector<ResultKind> Result;
+
+  ClangCompleter();
+  ~ClangCompleter();
+
+  void Reparse(const std::string& file, const std::string& content);
+  bool HasFile(const std::string& file);
+  void AddFile(const std::string& file, const ArgumentManager& arg_manager);
+  void AddFile(const std::string& file, const std::string& content,
+               const ArgumentManager& arg_manager);
+  std::string GetFileContent(const std::string& file);
+  std::vector<Result> CodeComplete(const std::string& file,
+                                   const std::string& content, int line,
+                                   int column, const ArgumentManager& arg_manager);
+
+  int file_count() const { return files_.size(); }
+
+ private:
+  Result GetResult(CXCompletionString cs);
+
+  CXIndex index_;
+  int parse_option_;
+  int complete_option_;
+
+  std::vector<std::string> files_;
+  std::vector<CXTranslationUnit> tus_;
+};
