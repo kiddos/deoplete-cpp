ae28660a8a8c8d1af2d66b943bab340f3676ec84
kiddos <kiddo831007@gmail.com>
2019-02-08 00:15:59 +0800

    add pio support for c

--- rplugin/python3/deoplete/sources/c.py
+++ rplugin/python3/deoplete/sources/c.py
@@ -56,6 +56,7 @@ class Source(Base, ClangDeopleteSourceBase):
     argument_manager.SetCStandard(standard)
 
     self.maybe_add_kernel_include_path(v, argument_manager)
+    self.maybe_add_pio_include_paths(v, argument_manager)
     return argument_manager
 
   def maybe_add_kernel_include_path(self, v, argument_manager):
@@ -64,6 +65,43 @@ class Source(Base, ClangDeopleteSourceBase):
     if kernel_dev:
       argument_manager.AddIncludePath(kernel_root)
       argument_manager.AddIncludePath(os.path.join(kernel_root, 'include'))
+
+  def maybe_add_pio_include_paths(self, v, argument_manager):
+    pio_dev = v['deoplete#sources#c#enable_platformio_dev']
+    pio_root = os.path.expanduser(v['deoplete#sources#c#platformio_root'])
+
+    if pio_dev and os.path.isdir(pio_root):
+      avr_path = os.path.join(pio_root,
+          'packages', 'framework-arduinoavr', 'cores', 'arduino')
+      if os.path.isdir(avr_path):
+        argument_manager.AddIncludePath(avr_path)
+
+      # stm32
+      arduino_stm32 = os.path.join(pio_root, 'packages', 'framework-arduinostm32')
+      if os.path.isdir(arduino_stm32):
+        for p in os.listdir(arduino_stm32):
+          if p not in ['.', '..']:
+            stm32_platform = os.path.join(arduino_stm32, p)
+            if os.path.isdir(stm32_platform):
+              stm32_path = os.path.join(stm32_platform, 'cores', 'maple')
+              argument_manager.AddIncludePath(stm32_path)
+
+      stm32_cube = os.path.join(pio_root, 'packages', 'framework-stm32cube')
+      if os.path.isdir(stm32_cube):
+        for p in os.listdir(stm32_cube):
+          if p not in ['.', '..']:
+            stm32_platform = os.path.join(stm32_cube, p)
+            if os.path.isdir(stm32_platform):
+              stm32_path = os.path.join(stm32_platform, 'Drivers',
+                  'STM32%sxx_HAL_Driver' % (p.upper()), 'Inc')
+              stm32_path = os.path.join(stm32_platform, 'Drivers'
+                  'CMSIS', 'Include')
+              argument_manager.AddIncludePath(stm32_path)
+
+      # mbed
+      mbed_path = os.path.join(pio_root, 'packages', 'framework-mbed')
+      if os.path.isdir(mbed_path):
+        argument_manager.AddIncludePath(mbed_path)
 
   def on_event(self, context):
     self.update(context)
