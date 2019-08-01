c4c9d373534f1f94303ecb65ac91cc9acafbf6e0
kiddos <kiddo831007@gmail.com>
2019-08-02 00:08:32 +0800

    try faster buffer getter

--- rplugin/python3/deoplete/clang_source_base.py
+++ rplugin/python3/deoplete/clang_source_base.py
@@ -28,21 +28,23 @@ class ClangCompletionWrapper(object):
     self._runner = None
     self._updating = False
 
+  def update_sync(self, filepath, content):
+    self._updating = True
+    self._completer.Parse(filepath, content, self._arg_manager)
+    self._completer.Update(self._arg_manager)
+    self._updating = False
+
   def update_async(self, filepath, content):
     """
     reparse the translation unit in completer
     if the translation unit is not in the completer
     the completer will add the translation unit
     """
-    self._completer.Parse(filepath, content, self._arg_manager)
-
-    def update_func():
-      self._completer.Update(self._arg_manager)
-      self._updating = False
-
     if not self._updating:
       self._updating = True
-      self._runner = threading.Thread(target=update_func)
+      self._runner = threading.Thread(target=self.update_sync,
+                                      args=(filepath,content,))
+      self._runner.daemon = True
       self._runner.start()
 
   def process_clang_result(self, result):
@@ -99,6 +101,10 @@ class ClangDeopleteSourceBase(object):
     """
     return self.vim.eval('expand("%:p")')
 
+  def get_buffer_content(self):
+    #  return self.vim.eval('join(getline(1, "$"), "\n")')
+    return '\n'.join(self.vim.current.buffer)
+
   def search_for_includes(self):
     current_dir = self.vim.command_output('pwd')
     includes = glob.glob(current_dir + '/**/include')
@@ -118,7 +124,7 @@ class ClangDeopleteSourceBase(object):
     """
     if self._completer:
       filepath = self.get_buffer_name()
-      content = '\n'.join(self.vim.current.buffer)
+      content = self.get_buffer_content()
       self._completer.update_async(filepath, content)
 
   def get_candidates(self, context):
@@ -129,6 +135,6 @@ class ClangDeopleteSourceBase(object):
       line = self.vim.eval('line(".")')
       col = self.vim.eval('col(".")')
       filepath = self.get_buffer_name()
-      content = '\n'.join(self.vim.current.buffer)
+      content = self.get_buffer_content()
       return self._completer.code_complete(filepath, content, line, col)
     return []
